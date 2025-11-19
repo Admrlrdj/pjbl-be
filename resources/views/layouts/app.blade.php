2ae0ea2621f4699b6bae059f8c9c11684fea2a58
fazamumtaz <fazamumtazramadhan@gmail.com>
2025-11-20 00:00:29 +0700

    feat: contact us

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Nounoufood - Cemilan Berkualitas')</title>
+
     
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
@@ -13,6 +14,9 @@
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
+    <!-- App CSS -->
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
     
     <style>
         body {
---
+++ resources/views/products.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Nounoufood - Our Story')
+
+@section('content')
+<section><h1>hai</h1></section>
+@endsection
